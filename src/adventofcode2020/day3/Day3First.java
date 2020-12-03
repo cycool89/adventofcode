b7499d7c94dbc27d76a844be97e43870bd1ccf00
Kigyós János <cycool89@gmail.com>
2020-12-03 13:33:24 +0100

    add day3First solution

---
+++ src/adventofcode2020/day3/Day3First.java
@@ -0,0 +1,30 @@
+package src.adventofcode2020.day3;
+
+import java.util.List;
+
+import src.lib.FileHandler;
+
+public class Day3First {
+    public static void main(String[] args) {
+        String[] inputPath = { "day3", "input.txt" };
+        List<String> lines = FileHandler.readByLine(inputPath);
+
+        int x = 0;
+        int y = 0;
+
+        int treeCount = 0;
+
+        while (y < lines.size()) {
+            String line = lines.get(y);
+
+            if (line.charAt(x) == '#') {
+                treeCount++;
+            }
+
+            x = (x + 3) % line.length();
+            y += 1;
+        }
+
+        System.out.println("Found " + treeCount + " tree on the path!");
+    }
+}
---
+++ src/adventofcode2020/day3/Day3Second.java
@@ -0,0 +1,13 @@
+package src.adventofcode2020.day3;
+
+import java.util.List;
+
+import src.lib.FileHandler;
+
+public class Day3Second {
+    public static void main(String[] args) {
+        String[] inputPath = { "day3", "input.txt" };
+        List<String> lines = FileHandler.readByLine(inputPath);
+
+    }
+}
