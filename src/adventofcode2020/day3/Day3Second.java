48d0a8987296e9205c7827c790ff7a146ff88893
Kigyós János <cycool89@gmail.com>
2020-12-03 13:45:53 +0100

    add day3Second solution

--- src/adventofcode2020/day3/Day3Second.java
+++ src/adventofcode2020/day3/Day3Second.java
@@ -9,5 +9,37 @@ public class Day3Second {
         String[] inputPath = { "day3", "input.txt" };
         List<String> lines = FileHandler.readByLine(inputPath);
 
+        int multipliedTreeCounters = 1;
+
+        multipliedTreeCounters *= Day3Second.countTrees(1, 1, lines);
+        multipliedTreeCounters *= Day3Second.countTrees(3, 1, lines);
+        multipliedTreeCounters *= Day3Second.countTrees(5, 1, lines);
+        multipliedTreeCounters *= Day3Second.countTrees(7, 1, lines);
+        multipliedTreeCounters *= Day3Second.countTrees(1, 2, lines);
+
+        System.out.println("Found trees multiplied result is " + multipliedTreeCounters);
+    }
+
+    public static int countTrees(int slopeX, int slopeY, List<String> lines) {
+
+        int x = 0;
+        int y = 0;
+
+        int treeCount = 0;
+
+        while (y < lines.size()) {
+            String line = lines.get(y);
+
+            if (line.charAt(x) == '#') {
+                treeCount++;
+            }
+
+            x = (x + slopeX) % line.length();
+            y += slopeY;
+        }
+
+        System.out.println(treeCount + " tree found with slope (" + slopeX + ", " + slopeY + ")");
+
+        return treeCount;
     }
 }
