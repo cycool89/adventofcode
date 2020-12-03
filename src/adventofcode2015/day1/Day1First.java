e7500dcd841607933b644c69eb790634b595110a
Kigyós János <cycool89@gmail.com>
2020-12-03 15:44:04 +0100

    add 2015 day1First solution

--- src/adventofcode2015/day1/Day1First.java
+++ src/adventofcode2015/day1/Day1First.java
@@ -8,6 +8,14 @@ public class Day1First {
     public static void main(String[] args) {
         String[] inputPath = { "day1", "input.txt" };
         List<String> lines = FileHandler.readByLine(2015, inputPath);
+        String line = lines.get(0);
+
+        long up = line.chars().filter(ch -> ch == '(').count();
+        long down = line.chars().filter(ch -> ch == ')').count();
+
+        long result = up - down;
+
+        System.out.println("Santa will be on floor " + result);
 
     }
 }
