3c4f2c3117910f47ef5362692412399e207ddb76
Kigyós János <cycool89@gmail.com>
2020-12-03 15:50:04 +0100

    add 2015 day2Second solution

--- src/adventofcode2015/day1/Day1Second.java
+++ src/adventofcode2015/day1/Day1Second.java
@@ -8,6 +8,17 @@ public class Day1Second {
     public static void main(String[] args) {
         String[] inputPath = { "day1", "input.txt" };
         List<String> lines = FileHandler.readByLine(2015, inputPath);
+        String line = lines.get(0);
+
+        int floor = 0;
+        int step = 0;
+
+        while (floor >= 0 && step < line.length()) {
+            floor = floor + (line.charAt(step) == '(' ? 1 : -1);
+            step++;
+        }
+
+        System.out.println("Santa will enter the basement in step " + step);
 
     }
 }
