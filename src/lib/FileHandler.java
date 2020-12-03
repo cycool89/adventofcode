2f598103765482fcc56dca2f0285205bb009530f
Kigyós János <kigyos.janos@nexius.hu>
2020-12-03 09:12:38 +0100

    add FileHandler for handling files

---
+++ src/lib/FileHandler.java
@@ -0,0 +1,28 @@
+package src.lib;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+
+    private FileHandler() {
+    }
+
+    public static List<String> readByLine(String[] pathSegments) {
+        File file = new File("src" + File.separator + "adventofcode2020" + File.separator + String.join(File.separator, pathSegments));
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
+            String line = reader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
