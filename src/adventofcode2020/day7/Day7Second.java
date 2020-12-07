2decc98a35f5694ff70d954a8cb798d8975bb30a
Kigyós János <cycool89@gmail.com>
2020-12-07 10:18:25 +0100

    add 2020/7/1 solution

---
+++ src/adventofcode2020/day7/Day7Second.java
@@ -0,0 +1,14 @@
+package src.adventofcode2020.day7;
+
+import java.util.List;
+
+import src.lib.FileHandler;
+
+public class Day7Second {
+
+    public static void main(String[] args) {
+        String[] inputPath = { "day7", "input.txt" };
+        List<String> lines = FileHandler.readByLine(2020, inputPath);
+
+    }
+}
