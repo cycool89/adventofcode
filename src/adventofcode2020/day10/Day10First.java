25d00d3d620abcd970c8c4954ea6eba6e39ff6b6
Kigyós János <cycool89@gmail.com>
2020-12-10 11:49:44 +0100

    remove day9 junk

--- src/adventofcode2020/day10/Day10First.java
+++ src/adventofcode2020/day10/Day10First.java
@@ -11,37 +11,8 @@ public class Day10First {
 
         String[] inputPath = { "day10", "input.txt" };
         List<String> lines = FileHandler.readByLine(2020, inputPath);
-        long[] input = lines.stream().mapToLong(Long::parseLong).toArray();
-
-        int preamble = 25;
-        long result = firstWrongNumber(input, preamble);
-
-        System.out.println("The first number that does not follows the rule: " + result);
+        int[] input = lines.stream().mapToInt(Integer::parseInt).toArray();
 
         RunningTime.check();
-    }
-
-    private static long firstWrongNumber(long[] input, int preamble) {
-        long result = -1;
-        int checkableNumberIndex = preamble;
-
-        while (checkableNumberIndex < input.length && result == -1) {
-            int intervalLowerEnd = checkableNumberIndex - preamble;
-            int intervalUpperEnd = checkableNumberIndex - 1;
-
-            boolean foundPair = false;
-            for (int i = intervalLowerEnd; i < intervalUpperEnd && !foundPair; i++) {
-                for (int j = i + 1; j <= intervalUpperEnd && !foundPair; j++) {
-                    if (input[i] != input[j] && (input[i] + input[j]) == input[checkableNumberIndex]) {
-                        foundPair = true;
-                    }
-                }
-            }
-
-            result = !foundPair ? input[checkableNumberIndex] : -1;
-            checkableNumberIndex++;
-        }
-
-        return result;
     }
 }
