9a553e3a79757db675908862a19d68083a2ae0db
cycool89 <cycool89@gmail.com>
2020-12-12 10:25:37 +0100

    2020/10/2 first try

--- src/adventofcode2020/day10/Day10Second.java
+++ src/adventofcode2020/day10/Day10Second.java
@@ -10,12 +10,33 @@ import src.lib.RunningTime;
 public class Day10Second {
 
     public static void main(String[] args) {
-        RunningTime.start();
+       RunningTime.start();
 
         String[] inputPath = { "day10", "input.txt" };
         List<String> lines = FileHandler.readByLine(2020, inputPath);
-        int[] input = lines.stream().mapToInt(Integer::parseInt).toArray();
+        int[] input = lines.stream().mapToInt(Integer::parseInt).sorted().toArray();
+
+        long variationCount = backtrack(input, 0, 0);
+
+        System.out.println("Total number of distinct ways: "
+                + variationCount;
 
         RunningTime.check();
+    }
+    
+    private static long backtrack(int[] input, int prev, int i) {
+     if (i >= input.length) {
+      return 1L;
+     }
+     if (input[i] - prev > 3) {
+      return 0L;
+     }
+     
+     long count = 0;
+     count += backtrack(input, input[i], i + 1);
+     count += backtrack(input, input[i], i + 2);
+     count += backtrack(input, input[i], i + 3);
+     
+     return count;
     }
 }
