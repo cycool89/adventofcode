055c9028e52ffec2709040f8741745de29143cb6
Kigyós János <cycool89@gmail.com>
2021-12-03 08:02:26 +0100

    Add 2021 / day1 / 1 solution

---
+++ src/adventofcode2021/day1/MeasurementCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    public class MeasurementCounter
+    {
+        private string[] lines;
+
+        public MeasurementCounter(string fileName)
+        {
+            lines = File.ReadAllLines(fileName);
+        }
+
+        public int Count()
+        {
+            int countIncreasedMeasurements = 0;
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var prevMeasurement = Convert.ToInt32(lines[i-1]);
+                var actMeasurement = Convert.ToInt32(lines[i]);
+                if (actMeasurement > prevMeasurement)
+                {
+                    countIncreasedMeasurements++;
+                }
+            }
+
+            return countIncreasedMeasurements;
+        }
+    }
+}
--- src/adventofcode2021/day1/Program.cs
+++ src/adventofcode2021/day1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using day1;
+
+int sol1demo = new MeasurementCounter("./input_1_demo.dat").Count();
+int sol1 = new MeasurementCounter("./input_1.dat").Count();
+
+Console.WriteLine("[1 DEMO] " + sol1demo);
+Console.WriteLine("[1 SOL ] " + sol1);
+
+
