cf907bc37865e9160baf3be431ffb60a516e7917
Kigyós János <cycool89@gmail.com>
2021-12-03 15:24:44 +0100

    Add 2021 / day2 / 2 solution

--- src/adventofcode2021/day2/Program.cs
+++ src/adventofcode2021/day2/Program.cs
@@ -6,10 +6,21 @@ using day2;
 Submarine submarine1Demo = new Submarine("./input1_demo.dat", 0, 0);
 Submarine submarine1 = new Submarine("./input1.dat", 0, 0);
 
-submarine1Demo.Execute();
+submarine1Demo.Execute(1);
 var result1Demo = submarine1Demo.Depth * submarine1Demo.Horizontal;
-submarine1.Execute();
+submarine1.Execute(1);
 var result1 = submarine1.Depth * submarine1.Horizontal;
 
 Console.WriteLine("[1 DEMO] " + result1Demo);
 Console.WriteLine("[1     ] " + result1);
+
+Submarine submarine2Demo = new Submarine("./input1_demo.dat", 0, 0, 0);
+Submarine submarine2 = new Submarine("./input1.dat", 0, 0, 0);
+
+submarine2Demo.Execute(2);
+var result2Demo = submarine2Demo.Depth * submarine2Demo.Horizontal;
+submarine2.Execute(2);
+var result2 = submarine2.Depth * submarine2.Horizontal;
+
+Console.WriteLine("[2 DEMO] " + result2Demo);
+Console.WriteLine("[2     ] " + result2);
--- src/adventofcode2021/day2/Submarine.cs
+++ src/adventofcode2021/day2/Submarine.cs
@@ -7,27 +7,36 @@ namespace day2
     {
         public int Depth { get; private set; }
         public int Horizontal { get; private set; }
+        public int Aim { get; private set; }
         private readonly string[] _data;
 
-        public Submarine(string inputFile, int depth, int horizontal)
+        public Submarine(string inputFile, int depth, int horizontal, int aim = 0)
         {
             _data = File.ReadAllLines(inputFile);
             Depth = depth;
             Horizontal = horizontal;
+            Aim = aim;
         }
 
-        public void Execute()
+        public void Execute(int whichPartOfDay)
         {
             foreach (var line in _data)
             {
                 var task = line.Split(" ");
                 Enum.TryParse(task[0], true, out Directions direction);
                 Int32.TryParse(task[1], out var amount);
-                Move(direction, amount);
+                if (whichPartOfDay == 1)
+                {
+                    Move1(direction, amount);
+                }
+                else
+                {
+                    Move2(direction, amount);
+                }
             }
         }
 
-        private void Move(Directions direction, int amount)
+        private void Move1(Directions direction, int amount)
         {
             switch (direction)
             {
@@ -39,6 +48,25 @@ namespace day2
                     break;
                 case Directions.Up:
                     Depth -= amount;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+        }
+
+        private void Move2(Directions direction, int amount)
+        {
+            switch (direction)
+            {
+                case Directions.Forward:
+                    Horizontal += amount;
+                    Depth += Aim * amount;
+                    break;
+                case Directions.Down:
+                    Aim += amount;
+                    break;
+                case Directions.Up:
+                    Aim -= amount;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
